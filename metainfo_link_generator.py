730e97b1fb49daea70370ccb61a42911e7fb7d0e
lprot <lprot@list.ru>
2021-06-03 03:22:26 +0200

    Duplicated sections fix

--- metainfo_link_generator.py
+++ metainfo_link_generator.py
@@ -15,10 +15,10 @@ if missing:
 import configparser, os
 
 if len(sys.argv) < 5:
- print("Usage: " + os.path.basename(__file__) + " <inputfile> <outputfile> <ID for change> <ID to change>")
- print("Example: " + os.path.basename(__file__) + " metainfo2.txt metainfo2.out 20 14")
- input("\nPress Enter to exit...")
- sys.exit(1) 
+    print("Usage: " + os.path.basename(__file__) + " <inputfile> <outputfile> <ID for change> <ID to change>")
+    print("Example: " + os.path.basename(__file__) + " metainfo2.txt metainfo2.out 20 14")
+    input("\nPress Enter to exit...")
+    sys.exit(1) 
 
 config = configparser.ConfigParser()
 config.optionxform = str
@@ -28,19 +28,16 @@ config2 = configparser.ConfigParser()
 config2.optionxform = str
 
 for section in config.sections():
- if "\\" + sys.argv[3] + "\\" in section:
-  newsection = section.replace("\\" + sys.argv[3] + "\\", "\\" + sys.argv[4] + "\\")
-  config2.add_section(section)
-  for option in config.options(section):
-   config2.set(section, option.title(), config.get(section, option))
-  config2.add_section(newsection)
-  config2.set(newsection, "Link", '"'+section+'"')
- else:
-  config2.add_section(section)
-  for option in config.options(section):
-   config2.set(section, option, config.get(section, option))
+    config2.add_section(section)
+    for option in config.options(section):
+        config2.set(section, option, config.get(section, option))
+    if "\\" + sys.argv[3] + "\\" in section:
+        newsection = section.replace("\\" + sys.argv[3] + "\\", "\\" + sys.argv[4] + "\\")
+        if not config2.has_section(newsection):
+            config2.add_section(newsection)
+            config2.set(newsection, "Link", '"' + section + '"')
 
 with open(sys.argv[2], "w") as config_file:
- config2.write(config_file)
+    config2.write(config_file)
 
 input("\nDone. Press Enter to exit...")
