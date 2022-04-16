3cc53fa36f9bbd924fd3b5ea0c06ce21484cc0fe
lprot <lprot@list.ru>
2022-04-16 03:45:22 +0200

    Compatibility fixes.

--- swap_patcher.py
+++ swap_patcher.py
@@ -1,37 +1,44 @@
+import sys
+if sys.version_info[0] < 3:
+    raw_input("You need to run this with Python 3!\nPress Enter to exit...")
+    sys.exit(1)
+    
 import os, re
 
 #Format: name of the patch, search pattern, offset, patch
-patches = ['SWaP signature check', b'\x00\x00\x50\xE3\x8D\x00\x00\x1A', 0, b'\x01\x00\xA0\xE3\x8D\x00\x00\xEA',
-           'VCRN matching check', b'\x00\x00\x54\xE3\x55\x00\x00\x1A', 0, b'\x01\x40\xA4\xE3\x55\x00\x00\xEA',
-           'Exception list signature check', b'\xEB\x00\x00\x50\xE3\x8A\x00\x00\x0A', 1, b'\x07',
-           'Setting RPMB key check', b'\xEB\x00\x00\x50\xE3\x25\x00\x00\x0A', 1, b'\x07']
+patches = ['Setting RPMB key check', b'\\xEB\\x00\\x00\\x50\\xE3\\x25\\x00\\x00\\x0A\\x54', 1, b'\\x07',
+           'Exception list signature check', b'\\xEB\\x00\\x00\\x50\\xE3\\x8A\\x00\\x00\\x0A', 1, b'\\x07',
+           'SWaP signature check', b'\\x00\\x00\\x50\\xE3\\x8D\\x00\\x00\\x1A', 0, b'\\x07',
+           'SWaP signature check fw1xx', b'\\xEB\\x00\\x00\\x50\\xE3\\x74\\x00\\x00\\x1A', 1, b'\\x07',
+           'VCRN matching check fw1xx', b'\\x00\\x00\\x54\\xE3\\xB4\\x00\\x00\\x1A', 0, b'\\x07',
+           'Setting RPMB key check fw1xx', b'\\xEB\\x00\\x00\\x50\\xE3\\x21\\x00\\x00\\x0A\\x11', 1, b'\\x07',
+           'Exception list signature check fw1xx', b'\\x00\\x00\\x50\\xE3\\x22\\x00\\x00\\x1A\\x2F', 0, b'\\x07',
+           'VCRN matching check', b'\\x00\\x00\\x54\\xE3\\x55\\x00\\x00\\x1A', 0, b'\\x07',
+           'Exception list signature check CN', b'\\xEB\\x00\\x00\\x50\\xE3\\x8A\\x00\\x00\\x1A\\xB8', 8, b'\\xE3']
 
 f = open('tsd.mibstd2.system.swap', 'rb')
-data = f.read()
+data = bytearray(f.read())
+print('File size:', f.tell(), 'bytes')
 f.close()
-
-f = open("tsd.mibstd2.system.swap.patched", 'w+b')
-f.write(data)
-flog = open("tsd.mibstd2.system.swap.patchlog.txt", 'w')
 
 num_of_applied_patches = 0
 num_of_patches = int(len(patches)/4)
 for i in range(num_of_patches):
   search_pattern = patches[i * 4 + 1]
   for match in re.compile(search_pattern).finditer(data):
-   offset = match.start()
-   length = match.end() - offset
-   f.seek(offset)
-   oldbytes = f.read(length)
-   newbytes = oldbytes[0:patches[i * 4 + 2]] + patches[i * 4 + 3] + oldbytes[len(patches[i * 4 + 3]) + patches[i * 4 + 2]:length]
-   print(patches[i * 4] + ' found at ' + hex(offset).upper().replace('0X', '0x') + '. Changing ' + oldbytes.hex().upper() + ' to ' + newbytes.hex().upper())
-   flog.writelines(hex(offset).upper().replace('0X', '0x') + ': ' + oldbytes.hex().upper() + ' ' + newbytes.hex().upper() + '\n')
+   offset = match.start() + patches[i * 4 + 2]
+   print(patches[i * 4] + ' found at ' + hex(offset).upper().replace('0X', '0x'))
+   for j in range(len(patches[i * 4 + 3])):
+    data[offset + j] = patches[i * 4 + 3][j]
    num_of_applied_patches += 1
-   f.seek(offset)
-   f.write(newbytes)
+   
+f = open("tsd.mibstd2.system.swap.patched", 'w+b')
+f.write(data)
 f.close()
 
 if num_of_patches == num_of_applied_patches:
  print('\nAll patches are applied :)')
 else:
  print('\nWARNING! Not all patches are applied!')
+ 
+input("\nPress Enter to exit...")
--- swdl_patcher.py
+++ swdl_patcher.py
@@ -6,37 +6,35 @@ if sys.version_info[0] < 3:
 import os, re
 
 #Format: name of the patch, search pattern, offset, patch
-patches = ['metainfo2.txt signature (aSignatureOfMet) check', b'\x00\x00\x50\xE3\x15\x00\x00\x0A\x54\x17\x9F\xE5', 0, b'\x07',
-           'metainfo2.txt checksum (aParseErrorInva_1) check', b'\xE3.\xFD\xFF\x1A..\x1F\xE5', 4, b'\xEA',
-           'metainfo2.txt signature (aParseErrorInva_0) check', b'\x1B\x00\x00\x1A..\x1F\xE5', 3, b'\xEA',
-           'Dependencies of update not met (aDependenciesOf) check', b'..\xFD\xEB\x00\x00\x50\xE3\x30\x00\x00\x1A', 4, b'\x07']
+patches = ['metainfo2.txt signature (aSignatureOfMet) check', b'.\\x00\\x56\\xE3\\x7D\\x00\\x00\\x0A\\x52\\x0F\\x4B\\xE2', 0, b'\x07',
+           'metainfo2.txt signature (aParseErrorInva_0) check', b'.\\x00\\x54\\xE3\\x1B\\x00\\x00...\\x1F\\xE5', 0, b'\x07',
+           'metainfo2.txt checksum (aParseErrorInva_1) check', b'.\\x00\\x50\\xE3.\\xFD\\xFF...\\x1F\\xE5', 0, b'\x07',
+           'Dependencies of update not met (aDependenciesOf) check', b'..\\xFD\\xEB.\\x00\\x50\\xE3\\x30\\x00\\x00\\x1A\\x04\\x00', 4, b'\x07',
+           'metainfo2.txt signature (aSignatureOfMet) check fw1xx', b'\\x00\\x00\\x53\\xE3\\x19\\x00\\x00\\x0A\\x52\\x0E\\x4B\\xE2', 0, b'\x07',
+           'metainfo2.txt signature (aParseErrorInva_0) check fw1xx', b'\\x00\\x00\\x54\\xE3\\x29\\x00\\x00\\x1A\\x5C\\x14\\x9F\\xE5', 0, b'\x07',
+           'metainfo2.txt signature (aParseErrorInva_1) check fw1xx', b'\\x00\\x00\\x50\\xE3\\x2D\\xFE\\xFF\\x1A\\x54\\x13\\x9F\\xE5', 0, b'\x07']
 
 f = open('tsd.mibstd2.system.swdownload', 'rb')
-data = f.read()
+data = bytearray(f.read())
+print('File size:', f.tell(), 'bytes')
 f.close()
-
-f = open("tsd.mibstd2.system.swdownload.patched", 'w+b')
-f.write(data)
-flog = open("tsd.mibstd2.system.swdownload.patchlog.txt", 'w')
 
 num_of_applied_patches = 0
 num_of_patches = int(len(patches)/4)
 for i in range(num_of_patches):
   search_pattern = patches[i * 4 + 1]
   for match in re.compile(search_pattern).finditer(data):
-   offset = match.start()
-   length = match.end() - offset
-   f.seek(offset)
-   oldbytes = f.read(length)
-   newbytes = oldbytes[0:patches[i * 4 + 2]] + patches[i * 4 + 3] + oldbytes[len(patches[i * 4 + 3]) + patches[i * 4 + 2]:length]
-   print(patches[i * 4] + ' found at ' + hex(offset).upper().replace('0X', '0x') + '. Changing ' + oldbytes.hex().upper() + ' to ' + newbytes.hex().upper())
-   flog.writelines(hex(offset).upper().replace('0X', '0x') + ': ' + oldbytes.hex().upper() + ' ' + newbytes.hex().upper() + '\n')
+   offset = match.start() + patches[i * 4 + 2]
+   print(patches[i * 4] + ' found at ' + hex(offset).upper().replace('0X', '0x'))
+   for j in range(len(patches[i * 4 + 3])):
+    data[offset + j] = patches[i * 4 + 3][j]
    num_of_applied_patches += 1
-   f.seek(offset)
-   f.write(newbytes)
+
+f = open("tsd.mibstd2.system.swdownload.patched", 'w+b')
+f.write(data)
 f.close()
 
-if num_of_patches == num_of_applied_patches:
+if num_of_applied_patches >= 3:
  print('\nAll patches are applied :)')
 else:
  print('\nWARNING! Not all patches are applied!')
