1572c02caca819496b98d1e5a02e31d5d1bc6001
lprot <lprot@list.ru>
2022-04-16 11:53:28 +0200

    Fixed typos.

--- swap_patcher.py
+++ swap_patcher.py
@@ -6,15 +6,15 @@ if sys.version_info[0] < 3:
 import os, re
 
 #Format: name of the patch, search pattern, offset, patch
-patches = ['Setting RPMB key check', b'\\xEB\\x00\\x00\\x50\\xE3\\x25\\x00\\x00\\x0A\\x54', 1, b'\\x07',
-           'Exception list signature check', b'\\xEB\\x00\\x00\\x50\\xE3\\x8A\\x00\\x00\\x0A', 1, b'\\x07',
-           'SWaP signature check', b'\\x00\\x00\\x50\\xE3\\x8D\\x00\\x00\\x1A', 0, b'\\x07',
-           'SWaP signature check fw1xx', b'\\xEB\\x00\\x00\\x50\\xE3\\x74\\x00\\x00\\x1A', 1, b'\\x07',
-           'VCRN matching check fw1xx', b'\\x00\\x00\\x54\\xE3\\xB4\\x00\\x00\\x1A', 0, b'\\x07',
-           'Setting RPMB key check fw1xx', b'\\xEB\\x00\\x00\\x50\\xE3\\x21\\x00\\x00\\x0A\\x11', 1, b'\\x07',
-           'Exception list signature check fw1xx', b'\\x00\\x00\\x50\\xE3\\x22\\x00\\x00\\x1A\\x2F', 0, b'\\x07',
-           'VCRN matching check', b'\\x00\\x00\\x54\\xE3\\x55\\x00\\x00\\x1A', 0, b'\\x07',
-           'Exception list signature check CN', b'\\xEB\\x00\\x00\\x50\\xE3\\x8A\\x00\\x00\\x1A\\xB8', 8, b'\\xE3']
+patches = ['Setting RPMB key check', b'\\xEB\\x00\\x00\\x50\\xE3\\x25\\x00\\x00\\x0A\\x54', 1, b'\x07',
+           'Exception list signature check', b'\\xEB\\x00\\x00\\x50\\xE3\\x8A\\x00\\x00\\x0A', 1, b'\x07',
+           'SWaP signature check', b'\\x00\\x00\\x50\\xE3\\x8D\\x00\\x00\\x1A', 0, b'\x07',
+           'SWaP signature check fw1xx', b'\\xEB\\x00\\x00\\x50\\xE3\\x74\\x00\\x00\\x1A', 1, b'\x07',
+           'VCRN matching check fw1xx', b'\\x00\\x00\\x54\\xE3\\xB4\\x00\\x00\\x1A', 0, b'\x07',
+           'Setting RPMB key check fw1xx', b'\\xEB\\x00\\x00\\x50\\xE3\\x21\\x00\\x00\\x0A\\x11', 1, b'\x07',
+           'Exception list signature check fw1xx', b'\\x00\\x00\\x50\\xE3\\x22\\x00\\x00\\x1A\\x2F', 0, b'\x07',
+           'VCRN matching check', b'\\x00\\x00\\x54\\xE3\\x55\\x00\\x00\\x1A', 0, b'\x07',
+           'Exception list signature check CN', b'\\xEB\\x00\\x00\\x50\\xE3\\x8A\\x00\\x00\\x1A\\xB8', 8, b'\xE3']
 
 f = open('tsd.mibstd2.system.swap', 'rb')
 data = bytearray(f.read())
