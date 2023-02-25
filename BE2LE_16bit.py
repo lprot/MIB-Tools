5414380ec9dc96b27c28fa6c5b44c4a49882cbe7
lprot <lprot@list.ru>
2023-02-25 14:20:51 +0100

    Cleaned a little bit

--- BE2LE_16bit.py
+++ BE2LE_16bit.py
@@ -1,15 +1,7 @@
 import sys
-
-infile = sys.argv[1]
-outfile = sys.argv[2]
-
-a = open(infile, "rb")
-chunk = a.read()
+chunk = open(sys.argv[1], "rb").read()
 converted = bytearray([])
 for i in range(int(len(chunk)/2)):
    converted += bytearray([ chunk[i*2+1], chunk[i*2] ])
 
-b = open(outfile , "wb")
-b.write(converted)
-b.close()
-a.close()
+open(sys.argv[2] , "wb").write(converted)
--- BE2LE_8bit.py
+++ BE2LE_8bit.py
@@ -1,10 +1,5 @@
-import os, sys, hashlib
-import struct
-
-infile = sys.argv[1]
-outfile = sys.argv[2]
-
-with open(infile, "rb") as old, open(outfile , "wb") as new:
+import sys, struct
+with open(sys.argv[1], "rb") as old, open(sys.argv[2] , "wb") as new:
     for chunk in iter(lambda: old.read(4), b""):
         chunk = struct.pack("<f", struct.unpack(">f", chunk)[0])
         new.write(chunk)
