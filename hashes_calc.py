8e8922b621d7789f453de98b1e19ee7030c40b1d
lprot <lprot@list.ru>
2023-05-07 09:39:11 +0200

    Code cleaning

--- hashes_calc.py
+++ hashes_calc.py
@@ -5,39 +5,41 @@ if sys.version_info[0] < 3:
 
 import os, hashlib 
 
-if not os.path.exists('tsd'):
-    input("\nERROR: Cannot find tsd folder.\n\nPress Enter to exit...")
-    sys.exit(1) 
-
 if os.path.sep == "\\":
-  eol = '\n'
+    eol = '\n'
 else:
-  eol = '\r\n'
+    eol = '\r\n'
+
+if len(sys.argv) < 2 and not os.path.exists('tsd'):
+    print("ERROR: Cannot find tsd folder!")
+    print(eol + "Usage: hash_calc.py <filename>")
+    input(eol +"Press Enter to exit...")
+    sys.exit(1)
 
 fout = open('hashes.txt','w')
-for dirpath, dirs, files in os.walk('tsd'):
+folder = 'tsd'
+if len(sys.argv) > 1:
+    folder = sys.argv[1]
+for dirpath, dirs, files in os.walk(folder):
   for filename in files:
     fname = os.path.join(dirpath,filename)
     with open(fname, 'rb') as f:
-      if fout.tell():
-        fout.write(eol)
-      fout.write('FileName = "/' + fname.replace(os.path.sep, '/') + '"' + eol)
-      fout.write('FileSize = "' + str(os.stat(fname).st_size) + '"' + eol)
-      i=0
-      while True:
-        sha1 = hashlib.sha1()
-        data = f.read(524288) #BUF_SIZE
-        if not data:
-         break
-        sha1.update(data)
-        if i>0:
-            n = str(i)
-        else:
-            n =''
-        fout.write('Checksum'+ n + ' = "' + sha1.hexdigest() + '"' + eol)
-        i+=1
+        if fout.tell():
+            fout.write(eol)
+        fout.write('FileName = "/' + fname.replace(os.path.sep, '/') + '"' + eol)
+        fout.write('FileSize = "' + str(os.stat(fname).st_size) + '"' + eol)
+        i=0
+        while True:
+            data = f.read(524288) #BUF_SIZE
+            if not data: break
+            sha1 = hashlib.sha1()
+            sha1.update(data)
+            if i > 0: n = str(i)
+            else: n =''
+            fout.write('Checksum'+ n + ' = "' + sha1.hexdigest() + '"' + eol)
+            i+=1
 if fout.tell():
- print('hashes.txt is recalculated :)')
+    input('hashes.txt is recalculated :)' + eol + eol + "Press Enter to exit...")
 else:
- print('Something went wrong :(')
+    input('Something went wrong :(' + eol + eol + "Press Enter to exit...")
 fout.close()
